9579b74cc2e5e15074962a69c30d1bf43c7f925c
adamDalley <adalley@gmail.com>
2025-08-17 08:18:07 -0700

    Added basic move to player

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ def main():
                 return
         screen.fill("black")
 
+        player.update(dt)
         player.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
-    
 
 if __name__ == "__main__":
     main()
