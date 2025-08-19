b7ce1f36d4e244b272c53b969ee95ae001759368
adamDalley <adalley@gmail.com>
2025-08-19 07:50:01 -0700

    Added shooting functionality

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 from circleshape import CircleShape
+from shot import Shot
 
 from constants import *
 
@@ -8,6 +9,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
 
         self.rotation = 0
+        self.shot_timer = 0
 
     def draw (self, screen_obj):
         pygame.draw.polygon(screen_obj, "white", self.triangle(), 2)  
@@ -25,6 +27,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        self.shot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -36,6 +39,18 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
 
+        if keys[pygame.K_SPACE]:
+            self.shoot()     
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        if self.shot_timer > 0:
+            return
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.shot_timer = PLAYER_SHOOT_COOLDOWN
+        
+        
---
+++ shot.py
@@ -0,0 +1,16 @@
+
+import pygame
+from circleshape import CircleShape
+
+from constants import *
+
+class Shot(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+
+    def draw (self, screen_obj):
+        pygame.draw.circle(screen_obj, "white", self.position, self.radius, 2) 
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+ 
